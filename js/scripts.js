90b243217081bc9dc6263508c357510eae9c3898
Michał Kupiecki <michal481216@gmail.com>
2018-03-28 23:10:11 +0200

    add carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// phone carousel elements
+var carouselLeftSlider = document.getElementsByClassName('js-leftSwitch');
+var carouselRightSlider = document.getElementsByClassName('js-rightSwitch');
+var reg = document.getElementById('js-numberOfReg');
+var numberOfReg = 1;
+
+//			scrollspy
 $(function() {
 	$('body').scrollspy({ target: '#navigation-bar' });
 
@@ -7,16 +14,38 @@ $(function() {
 
 	function changeNavbar() {
 		var home = $(document).scrollTop();
-		//console.log(home);
 		if (home >= 100) {
 			$('nav').addClass('sticky');
 		} else {
 			$('nav').removeClass('sticky');
 		}
     }
-    $('#designControls').on('slid.bs.carousel', function() {
-		var currentSlideName = $('#design-id');
-		var activeSlideNumber = $('div.active').data('number');
-		currentSlideName.text(activeSlideNumber);
+});
+//options carousel
+
+$(function() {
+	$('.carousel').carousel({
+		interval: 2000,
+		pause: true
 	});
+	$(".carousel").carousel("pause");
+});
+
+
+
+
+//			phone carousel switches	
+carouselRightSlider[0].addEventListener('click', function() {
+	numberOfReg++;
+	if (numberOfReg == 6) {
+		numberOfReg = 1;
+	}
+	reg.innerHTML = numberOfReg;
+});
+carouselLeftSlider[0].addEventListener('click', function() {
+	numberOfReg--;
+	if (numberOfReg == 0) {
+		numberOfReg = 5;
+	}
+	reg.innerHTML = numberOfReg;
 });
